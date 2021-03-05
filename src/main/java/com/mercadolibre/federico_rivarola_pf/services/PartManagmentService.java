674dad9e81023ed6bea1a30f4c8ef4b00773f4ce
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-05 16:15:44 -0300

    stockdto & partsmanagementservice implement

--- src/main/java/com/mercadolibre/federico_rivarola_pf/services/PartManagmentService.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/services/PartManagmentService.java
@@ -1,4 +1,43 @@
 package com.mercadolibre.federico_rivarola_pf.services;
 
-public class PartManagmentService {
+import com.mercadolibre.federico_rivarola_pf.dtos.PartDTO;
+import com.mercadolibre.federico_rivarola_pf.repositories.PartsRepository;
+import com.mercadolibre.federico_rivarola_pf.repositories.StockRepository;
+import com.mercadolibre.federico_rivarola_pf.services.interfaces.IPartManagementService;
+import com.mercadolibre.federico_rivarola_pf.util.enums.OrderType;
+import com.mercadolibre.federico_rivarola_pf.util.enums.Querytype;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class PartManagmentService implements IPartManagementService {
+    PartsRepository partsRepository;
+    StockRepository stockRepository;
+
+    public PartManagmentService(PartsRepository partsRepository, StockRepository stockRepository) {
+        this.partsRepository = partsRepository;
+        this.stockRepository = stockRepository;
+    }
+
+    @Override
+    public List<PartDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<PartDTO> getAllByQueryType() {
+        return null;
+    }
+
+    @Override
+    public List<PartDTO> getAllByQueryTypeAndDate(Querytype queryType, LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public List<PartDTO> getAllByQueryTypeAndDateSorter(Querytype queryType, LocalDate date, OrderType orderType) {
+        return null;
+    }
 }
