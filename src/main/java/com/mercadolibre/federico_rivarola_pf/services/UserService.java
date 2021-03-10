bbf165f7ed1f9accc158598e12b67ca5b870f614
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-10 17:07:32 -0300

    user services and auth services

--- src/main/java/com/mercadolibre/federico_rivarola_pf/services/UserService.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/services/UserService.java
@@ -1,38 +1,118 @@
 package com.mercadolibre.federico_rivarola_pf.services;
 
+import com.mercadolibre.federico_rivarola_pf.dtos.UserDTO;
 import com.mercadolibre.federico_rivarola_pf.model.Subsidiary;
 import com.mercadolibre.federico_rivarola_pf.model.User;
+import com.mercadolibre.federico_rivarola_pf.repositories.ISubsidiaryRepository;
 import com.mercadolibre.federico_rivarola_pf.repositories.IUserRepository;
 import com.mercadolibre.federico_rivarola_pf.services.interfaces.IUserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Service for user management
+ * @author frivarola
+ */
+@Service
 public class UserService implements IUserService {
     private final IUserRepository userRepository;
+    private final ISubsidiaryRepository subsidiaryRepository;
 
-    public UserService(IUserRepository userRepository) {
+    public UserService(IUserRepository userRepository, ISubsidiaryRepository subsidiaryRepository) {
         this.userRepository = userRepository;
+        this.subsidiaryRepository = subsidiaryRepository;
     }
 
+    /**
+     * method for create user
+     * @param username
+     * @param pwd
+     * @throws ResponseStatusException
+     */
+    @Override
+    public void createUser(String username, String pwd, String idSubsidiary) throws ResponseStatusException {
+        User u = new User();
+        u.setUsername(username);
+        u.setPassword(pwd);
+        u.setSubsidiary(subsidiaryRepository.findById(idSubsidiary));
+        if(u.getSubsidiary() != null){
+            userRepository.save(u);
+        }
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid subsidiary");
+
+    }
+
+    /**
+     * method for authenticate user
+     * @param username
+     * @param pwd
+     * @return UserDTO with JWToken
+     * @throws ResponseStatusException
+     */
+    @Override
+    public UserDTO authUser(String username, String pwd) throws ResponseStatusException {
+        UserDTO userDTO = null;
+
+        if (validateCredentials(username, pwd)) {
+            userDTO = new UserDTO();
+            String token = getJWTToken(username);
+            userDTO.setUsername(username);
+            userDTO.setToken(token);
+        }
+        return userDTO;
+    }
+
+    /**
+     * get all users on users table
+     *
+     * @return
+     */
     @Override
     public List<User> getAll() {
         return (List<User>) userRepository.findAll();
     }
 
+    /**
+     * get user by username
+     *
+     * @param username
+     * @return
+     */
     @Override
     public User getUserByUsername(String username) {
         return userRepository.findUser(username);
     }
 
+    /**
+     * get subsidiary by user
+     *
+     * @param username
+     * @return
+     */
     @Override
     public Subsidiary getSubsidiaryByUser(String username) {
         return userRepository.getSubsidiary(username);
     }
 
+    /**
+     * function for validate auth credentials
+     *
+     * @param username
+     * @param password
+     * @return
+     * @throws ResponseStatusException
+     */
     @Override
     public Boolean validateCredentials(String username, String password) throws ResponseStatusException {
         User u = userRepository.findUser(username);
@@ -43,6 +123,32 @@ public class UserService implements IUserService {
         }
         //invalid credentials for pwd incorrect, user not exist, etc..
         throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
+    }
+
+    /**
+     * method for create JWTToken
+     * @param username
+     * @return
+     */
+    private String getJWTToken(String username) {
+        String secretKey = "mySecretKey";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token = Jwts
+                .builder()
+                .setId("frivarolaJWT")
+                .setSubject(username)
+                .claim("authorities",
+                        grantedAuthorities.stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS512,
+                        secretKey.getBytes()).compact();
+
+        return "Bearer " + token;
     }
 
 
--- src/main/java/com/mercadolibre/federico_rivarola_pf/services/interfaces/IUserService.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/services/interfaces/IUserService.java
@@ -1,12 +1,15 @@
 package com.mercadolibre.federico_rivarola_pf.services.interfaces;
 
 
+import com.mercadolibre.federico_rivarola_pf.dtos.UserDTO;
 import com.mercadolibre.federico_rivarola_pf.model.Subsidiary;
 import com.mercadolibre.federico_rivarola_pf.model.User;
 
 import java.util.List;
 
 public interface IUserService {
+    void createUser(String username, String pwd, String idSubsidiary);
+    UserDTO authUser(String username, String pwd);
     List<User> getAll();
     User getUserByUsername(String username);
     Subsidiary getSubsidiaryByUser(String username);
