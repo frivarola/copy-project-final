d669e14b4254c66ee0c7cec99abe34b2f80f9002
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-10 17:07:58 -0300

    subsidiary repository

--- src/main/java/com/mercadolibre/federico_rivarola_pf/repositories/ISubsidiaryRepository.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/repositories/ISubsidiaryRepository.java
@@ -1,2 +1,13 @@
-package com.mercadolibre.federico_rivarola_pf.repositories;public interface ISubsidiaryRepository {
+package com.mercadolibre.federico_rivarola_pf.repositories;
+
+import com.mercadolibre.federico_rivarola_pf.model.Subsidiary;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISubsidiaryRepository extends CrudRepository<Subsidiary, Integer> {
+    @Query("SELECT s FROM Subsidiary s WHERE s.id = :idSubsidiary")
+    Subsidiary findById(@Param("idSubsidiary") String idSubsidiary);
 }
