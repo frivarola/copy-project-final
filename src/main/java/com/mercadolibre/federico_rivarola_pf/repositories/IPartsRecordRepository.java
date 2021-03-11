9fc2fb1a81d42049c41973bc84b0e389e2478bf0
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-11 15:51:22 -0300

    fix queries

--- src/main/java/com/mercadolibre/federico_rivarola_pf/repositories/IPartsRecordRepository.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/repositories/IPartsRecordRepository.java
@@ -14,10 +14,13 @@ import java.util.List;
 public interface IPartsRecordRepository extends CrudRepository<PartRecord, Integer> {
     @Query("SELECT pr FROM PartRecord pr")
     List<PartRecord> findAll();
+
     @Query("SELECT pr FROM PartRecord pr JOIN Part p ON pr.part = p WHERE p.id = :idPart")
     List<PartRecord> findByIdPart(@Param("idPart") String idPart);
-    @Query("SELECT pr FROM PartRecord pr WHERE to_date(pr.lastModification, 'yyyy-MM-dd') >= :date and pr.part.id in := idParts")
+
+    @Query("SELECT pr FROM PartRecord pr JOIN Part p ON pr.part = p WHERE p.id in :idParts and to_date(pr.lastModification, 'yyyy-MM-dd') >= :date")
     List<PartRecord> findByLastModificationAndIdsParts(@Param("date") LocalDate date, @Param("idParts") List<String> idParts);
+
     @Query("SELECT pr FROM PartRecord pr JOIN Part p ON pr.part = p WHERE p.id in :idParts")
     List<PartRecord> findByListIdParts(@Param("idParts") List<String> idParts);
 
