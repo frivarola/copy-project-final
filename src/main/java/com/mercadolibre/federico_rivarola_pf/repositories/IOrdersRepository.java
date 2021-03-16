c47b082eca8338bd7be76d6118e5ad94b52ee3a6
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-16 08:20:20 -0300

    fix query

--- src/main/java/com/mercadolibre/federico_rivarola_pf/repositories/IOrdersRepository.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/repositories/IOrdersRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 @Repository
 public interface IOrdersRepository extends CrudRepository<OrderCM, Integer> {
-    @Query("SELECT o FROM Orders o WHERE o.dealer.dealerNumber = :dealerNumber ")
+    @Query("SELECT o FROM OrderCM o WHERE o.dealer.dealerNumber = :dealerNumber ")
     List<OrderCM> findByDealerNumber(@Param("dealerNumber") String dealerNumber);
-    @Query("SELECT o FROM Orders o WHERE o.dealer.dealerNumber = :dealerNumber AND o.deliveryStatus.code = :deliveryStatus")
+    @Query("SELECT o FROM OrderCM o WHERE o.dealer.dealerNumber = :dealerNumber AND o.deliveryStatus.code = :deliveryStatus")
     List<OrderCM> findByDealerNumberAndDeliveryStatus(@Param("dealerNumber") String dealerNumber, @Param("deliveryStatus") String deliveryStatus);
 }
