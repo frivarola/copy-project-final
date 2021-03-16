d066de76e43688140bae0a83df497a1eca75f585
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-16 10:20:10 -0300

    refactor dtos orders

--- src/main/java/com/mercadolibre/federico_rivarola_pf/dtos/OrderDTO.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/dtos/OrderDTO.java
@@ -11,4 +11,46 @@ public class OrderDTO {
     private String deliveryStatus;
     private List<OrderDetailCM> orderDetails;
 
+    public OrderDTO() {
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Integer getDaysDelay() {
+        return daysDelay;
+    }
+
+    public void setDaysDelay(Integer daysDelay) {
+        this.daysDelay = daysDelay;
+    }
+
+    public String getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(String deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public List<OrderDetailCM> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetailCM> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
 }
--- src/main/java/com/mercadolibre/federico_rivarola_pf/dtos/responses/OrderResponseDTO.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/dtos/responses/OrderResponseDTO.java
@@ -8,4 +8,23 @@ import java.util.List;
 public class OrderResponseDTO {
     private String dealerNumber;
     private List<OrderDTO> orders;
+
+    public OrderResponseDTO() {
+    }
+
+    public String getDealerNumber() {
+        return dealerNumber;
+    }
+
+    public void setDealerNumber(String dealerNumber) {
+        this.dealerNumber = dealerNumber;
+    }
+
+    public List<OrderDTO> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<OrderDTO> orders) {
+        this.orders = orders;
+    }
 }
