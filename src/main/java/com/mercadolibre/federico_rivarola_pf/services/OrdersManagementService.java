f6f7e08edb88a7f49938b9b98cffe50c8ca8c09d
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-16 10:19:50 -0300

    refactor services ordersManagement

--- src/main/java/com/mercadolibre/federico_rivarola_pf/services/OrdersManagementService.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/services/OrdersManagementService.java
@@ -1,2 +1,106 @@
-package com.mercadolibre.federico_rivarola_pf.services;public class OrdersManagementService {
+package com.mercadolibre.federico_rivarola_pf.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.federico_rivarola_pf.dtos.OrderDTO;
+import com.mercadolibre.federico_rivarola_pf.dtos.responses.OrderResponseDTO;
+import com.mercadolibre.federico_rivarola_pf.model.Dealer;
+import com.mercadolibre.federico_rivarola_pf.model.OrderCM;
+import com.mercadolibre.federico_rivarola_pf.repositories.IDealerRepository;
+import com.mercadolibre.federico_rivarola_pf.repositories.IOrdersRepository;
+import com.mercadolibre.federico_rivarola_pf.services.interfaces.IOrdersManagementService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrdersManagementService implements IOrdersManagementService {
+    private final IOrdersRepository ordersRepository;
+    private final IDealerRepository dealerRepository;
+    private final ObjectMapper objectMapper;
+
+    public OrdersManagementService(IOrdersRepository ordersRepository, IDealerRepository dealerRepository, ObjectMapper objectMapper) {
+        this.ordersRepository = ordersRepository;
+        this.dealerRepository = dealerRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public OrderResponseDTO getAll() {
+        return null;
+    }
+
+    @Override
+    public OrderResponseDTO getByDealerNumber(String dealerNumber) throws ResponseStatusException {
+        String subsidiary = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Dealer d = dealerRepository.findDealerByIdSubsidiary(subsidiary);
+
+        if (d != null) {
+
+            OrderResponseDTO response = new OrderResponseDTO();
+            List<OrderCM> result = ordersRepository.findByDealerNumber(dealerNumber);
+
+            if (result != null) {
+                if (!result.isEmpty()) {
+
+                    List<OrderDTO> orderDTOs = convertToListOrderDTO(result);
+                    response.setDealerNumber(dealerNumber);
+                    response.setOrders(orderDTOs);
+
+                    return response;
+
+                }
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se encontraron resultados.");
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No existe dealer indicado para la consecionaria");
+
+    }
+
+    @Override
+    public OrderResponseDTO getByDealerNumberAndDeliveryStatus(String dealerNumber, String deliveryStatus) {
+        String subsidiary = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Dealer d = dealerRepository.findDealerByIdSubsidiary(subsidiary);
+
+        if (d != null) {
+
+            OrderResponseDTO response = new OrderResponseDTO();
+            List<OrderCM> result = ordersRepository.findByDealerNumberAndDeliveryStatus(dealerNumber, deliveryStatus);
+
+            if (result != null) {
+                if (!result.isEmpty()) {
+
+                    List<OrderDTO> orderDTOs = convertToListOrderDTO(result);
+                    response.setDealerNumber(dealerNumber);
+                    response.setOrders(orderDTOs);
+
+                    return response;
+
+                }
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se encontraron resultados.");
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No existe dealer indicado para la consecionaria");
+
+    }
+
+    private List<OrderDTO> convertToListOrderDTO(List<OrderCM> orders) {
+        List<OrderDTO> orderDTOS = new ArrayList<>();
+        OrderDTO orderDTO = new OrderDTO();
+        for (OrderCM o : orders) {
+            orderDTO.setOrderNumber(o.getOrderNumberCM());
+            orderDTO.setDaysDelay(o.getDaysDelayed());
+            orderDTO.setOrderDetails(o.getOrderDetails());
+            orderDTO.setDeliveryStatus(o.getDeliveryStatus().getCode());
+            orderDTO.setOrderDate(o.getOrderDate());
+
+            orderDTOS.add(orderDTO);
+        }
+        return orderDTOS;
+    }
 }
--- src/main/java/com/mercadolibre/federico_rivarola_pf/services/interfaces/IOrdersManagementService.java
+++ src/main/java/com/mercadolibre/federico_rivarola_pf/services/interfaces/IOrdersManagementService.java
@@ -1,2 +1,15 @@
-package com.mercadolibre.federico_rivarola_pf.services.interfaces;public interface IOrdersManagementService {
+package com.mercadolibre.federico_rivarola_pf.services.interfaces;
+
+import com.mercadolibre.federico_rivarola_pf.dtos.responses.OrderResponseDTO;
+import com.mercadolibre.federico_rivarola_pf.model.OrderCM;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IOrdersManagementService {
+    OrderResponseDTO getAll();
+    OrderResponseDTO getByDealerNumber(String dealerNumber);
+    OrderResponseDTO getByDealerNumberAndDeliveryStatus(String dealerNumber, String deliveryStatus);
+
 }
