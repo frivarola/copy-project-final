55b53692413d28eb096f80599dc416d20c20d7ca
Federico A. Rivarola <federico.rivarola@mercadolibre.com>
2021-03-16 15:44:45 -0300

    test req 04

--- src/test/java/com/mercadolibre/federico_rivarola_pf/integration/PartManagementControllerTest.java
+++ src/test/java/com/mercadolibre/federico_rivarola_pf/integration/PartManagementControllerTest.java
@@ -1,19 +1,25 @@
 package com.mercadolibre.federico_rivarola_pf.integration;
 
 import com.mercadolibre.federico_rivarola_pf.controller.PartManagementController;
-import com.mercadolibre.federico_rivarola_pf.repositories.IPartsRecordRepository;
-import com.mercadolibre.federico_rivarola_pf.repositories.IPartsRepository;
-import com.mercadolibre.federico_rivarola_pf.repositories.IProviderRepository;
-import com.mercadolibre.federico_rivarola_pf.repositories.IStockRepository;
+import com.mercadolibre.federico_rivarola_pf.repositories.*;
 import com.mercadolibre.federico_rivarola_pf.services.PartManagementService;
 import com.mercadolibre.federico_rivarola_pf.services.interfaces.IPartManagementService;
+import com.mercadolibre.federico_rivarola_pf.services.interfaces.IPartRecordService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @WebMvcTest(PartManagementController.class)
@@ -23,7 +29,18 @@ public class PartManagementControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private IPartManagementService service;
+    private IPartsRepository partsRepository;
+    @MockBean
+    private IStockRepository stockRepository;
+    @MockBean
+    private IPartsRecordRepository partsRecordRepository;
+    @MockBean
+    private ISubsidiaryRepository subsidiaryRepository;
+
+    @SpyBean
+    private PartManagementService service;
+    @InjectMocks
+    private PartManagementController controller;
 
     @Test
     void findWithValidUser(){
@@ -50,5 +67,34 @@ public class PartManagementControllerTest {
 
     }
 
+    @Test
+    void shouldFailSaveStock_InvalidPart() throws Exception {
+        //when(parts ).thenReturn();
+        this.mockMvc.perform(post("/api/v1/booking")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"idPart\": \"AAaa10\", \"quantity\": 100}"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldFailSaveStock_InvalidQuantity() throws Exception {
+        //when(parts ).thenReturn();
+        this.mockMvc.perform(post("/api/v1/booking")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"idPart\": \"AA10\", \"quantity\": -100}"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldWorkSaveStock() throws Exception {
+        //when(parts ).thenReturn();
+        this.mockMvc.perform(post("/api/v1/booking")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"idPart\": \"AA10\", \"quantity\": 100}"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
 }
